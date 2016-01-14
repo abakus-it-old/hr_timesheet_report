a5c1629ab3d1c959dcff6260541f2788087b45d4
Valentin Thirion <valentinthirion@gmail.com>
2016-01-14 10:56:45 +0100

    First working version

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Timesheet Report Printing",
+    'version': '1.0',
+    'depends': ['hr'],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'HR',
+    'description': 
+    """
+Print the timesheet as a beautiful and signable report.
+    """,
+    'data': [
+            'report/hr_timesheet_report.xml',
+             ],
+}
