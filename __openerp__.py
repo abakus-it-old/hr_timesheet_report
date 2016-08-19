9d2320b8ca179a165e4068854d0c9dfd7035bc65
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:04:49 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Timesheet Report Printing",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['hr'],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
